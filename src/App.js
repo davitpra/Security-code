bfae378fb7ac1d17b18d962ec102e5a1f46c4749
David <j.david.prado1716@gmail.com>
2022-08-29 15:06:20 -0500

    maquetacion de la aplicacion

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import { UseState } from "./UseState";
+import { ClassState } from "./ClassState";
+
+function App() {
+  return (
+    <div className="App">
+      <UseState/>
+      <ClassState/>
+    </div>
+  );
+}
+
+export default App;
